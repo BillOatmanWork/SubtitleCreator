b7617aa36a1c60b902f1f5ff33b33eaf617d6256
William Oatman <363964@wegmans.com>
2024-09-16 12:08:55 -0400

    Updated bat file to test

--- Doc/PostProcessRecordings.bat
+++ Doc/PostProcessRecordings.bat
@@ -5,4 +5,6 @@ c:
 cd\comskip
 comskip --ini=comskipper.ini "%drive%%folder%%filename%.ts"
 
-REM -ffmpegpath="C:\ffmpeg-2024-08-15-git-1f801dfdb5-full_build\bin" -infile="C:\TestData\Recipe Rehab S02E25.ts" -model=medium -nomerge
+cd\SubtitleCreator
+SubtitleCreator -ffmpegpath="C:\ffmpeg-2024-08-15-git-1f801dfdb5-full_build\bin" -infile="%drive%%folder%%filename%.ts" -model=medium -nomerge
+
