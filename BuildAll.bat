323ca5a5e04f97250214c251493796cf769da938
William Oatman <363964@wegmans.com>
2024-09-16 12:25:09 -0400

    Updated buildall.bat and added build folder

--- BuildAll.bat
+++ BuildAll.bat
@@ -3,16 +3,16 @@ dotnet publish -r osx.10.14-x64 -c Release /p:PublishSingleFile=true /p:IncludeN
 dotnet publish -r ubuntu.18.04-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained
 dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained
 
-cd \Stuff\HdHomerunM3U\test
+cd \repos\SubtitleCreator\Build
 
-copy /Y "C:\Stuff\Repos\HdHomerunM3U\bin\Release\net6.0\win-x64\publish\HdHomerunM3U.exe" .
-"C:\Program Files\7-Zip\7z" a -tzip HdHomerunM3U-WIN.zip HdHomerunM3U.exe
+copy /Y "C:\repos\SubtitleCreator\bin\Release\net8.0\win-x64\publish\SubtitleCreator.exe" .
+"C:\Program Files\7-Zip\7z" a -tzip SubtitleCreator-WIN.zip SubtitleCreator.exe
 
-copy /Y "C:\Stuff\Repos\HdHomerunM3U\bin\Release\net6.0\osx.10.14-x64\publish\HdHomerunM3U" .
-"C:\Program Files\7-Zip\7z" a -t7z HdHomerunM3U-OSX.7z HdHomerunM3U
+copy /Y "C:\repos\SubtitleCreator\bin\Release\net8.0\osx.10.14-x64\publish\SubtitleCreator" .
+"C:\Program Files\7-Zip\7z" a -t7z SubtitleCreator-OSX.7z SubtitleCreator
 
-copy /Y "C:\Stuff\Repos\HdHomerunM3U\bin\Release\net6.0\ubuntu.18.04-x64\publish\HdHomerunM3U" .
-"C:\Program Files\7-Zip\7z" a -t7z HdHomerunM3U-UBU.7z HdHomerunM3U
+copy /Y "C:\repos\SubtitleCreator\bin\Release\net8.0\ubuntu.18.04-x64\publish\SubtitleCreator" .
+"C:\Program Files\7-Zip\7z" a -t7z SubtitleCreator-UBU.7z SubtitleCreator
 
-copy /Y "C:\Stuff\Repos\HdHomerunM3U\bin\Release\net6.0\linux-x64\publish\HdHomerunM3U" .
-"C:\Program Files\7-Zip\7z" a -t7z HdHomerunM3U-LIN64.7z HdHomerunM3U
+copy /Y "C:\repos\SubtitleCreator\bin\Release\net8.0\linux-x64\publish\SubtitleCreator" .
+"C:\Program Files\7-Zip\7z" a -t7z SubtitleCreator-LIN64.7z SubtitleCreator
